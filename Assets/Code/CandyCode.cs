04bc0f1191fdbf7267c5ee0eedd42c4a7319ba3a
bhuvanXOXO <95708602+bhuvanXOXO@users.noreply.github.com>
2022-04-26 20:03:52 -0400

    made the world space setting for candy counter, added particles rising to each candy, so that the trail remains also after consuming candies, changed the player from being a dwarf in the scene, reduced castle/house height

--- Assets/Code/CandyCode.cs
+++ Assets/Code/CandyCode.cs
@@ -8,6 +8,7 @@ public class CandyCode : MonoBehaviour
 {
     public TextMeshProUGUI candyUI;
     public AudioSource crunchCandy;
+    public GameObject candyCounter;
     private void OnTriggerEnter(Collider other)
     {
          if(other.CompareTag("Player")){
@@ -15,6 +16,13 @@ public class CandyCode : MonoBehaviour
              PublicVars.candyNum++;
              candyUI.text = PublicVars.candyNum++.ToString();
              Destroy(gameObject);
+             StartCoroutine(DisplayCandyCount());
          }
+    }
+    private IEnumerator DisplayCandyCount()
+    {
+        candyCounter.SetActive(true);
+        yield return new WaitForSeconds(2);
+        candyCounter.SetActive(false);
     }
 }
